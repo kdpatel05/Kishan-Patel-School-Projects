b6bd36a1f078b82f7e1139d50aafda17944e5b3b
kdpatel05 <kishandpatel2005@gmail.com>
2025-09-27 12:57:37 -0400

    Delete finalproject student info app/finalproject directory

--- finalproject student info app/finalproject/README.md
+++
@@ -1,1 +0,0 @@
-This is the final project with the studentinfo application.
--- finalproject student info app/finalproject/studentinfo/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from studentinfo.models import Studentdetails, Coursedetails # Import Studentdetails and Coursedetails models
-
-# Register your models here.
-
-admin.site.register(Studentdetails)
-admin.site.register(Coursedetails)
